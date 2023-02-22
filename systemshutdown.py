d4212a9505f2a19fb5b2515d5d58c0fb1d059843
Deepak kumar <95753797+Kumardeepak1234@users.noreply.github.com>
2023-02-22 09:07:45 +0530

    python project

---
+++ systemshutdown.py
@@ -0,0 +1,14 @@
+import os
+import platform
+def shutdown():
+    if platform.system()=="Windows":
+        os.system("shutdown -s")
+    else:
+        print("os is not supported")
+
+command=input("enter s for shutdown")
+if command=="s":
+    shutdown()
+else:
+    print("wrong input")
+        
